73665702ebbe54794639751bc0be5fb3fea3a72c
PabloTorres <ptorresp@ups.edu.ec>
2025-11-11 01:13:12 -0500

    Agregar controlador y modelo de Person para validación de tests

--- src/controllers/PersonController.java
+++ src/controllers/PersonController.java
@@ -8,7 +8,7 @@ public class PersonController {
         int n = people.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (people[j].getName().compareTo(people[j + 1].getName()) > 0) {
+                if (people[j].getName().compareTo(people[j + 1].getName()) < 0) {
                     // swap people[j] and people[j+1]
                     Person temp = people[j];
                     people[j] = people[j + 1];
